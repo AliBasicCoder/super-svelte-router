826122ffda1b270f0e77c176bd576f184e3af78d
AliBasicCoder <aliahmedreda34@gmail.com>
2021-02-24 20:10:35 +0200

    fix formatting

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
 import { routerStore } from "./routerStore";
+
+/**
+ * @param {HTMLLinkElement} node
+ * @param {string} href
+ */
 export function link(node, href) {
-    href && (node.href = href);
-    node.addEventListener("click", linkHandler);
-    return {
-        destroy() {
-            node.removeEventListener("click", linkHandler);
-        },
-    };
+  href && (node.href = href);
+  node.addEventListener("click", linkHandler);
+  return {
+    destroy() {
+      node.removeEventListener("click", linkHandler);
+    },
+  };
 }
+
 /**
  * @deprecated use link action instead
+ * @param {MouseEvent} e
  */
 export function linkHandler(e) {
-    e.preventDefault();
-    routerStore.redirect(new URL(e.target?.href).pathname);
+  e.preventDefault();
+  routerStore.redirect(new URL(e.target.href).pathname);
 }
+
 /**
  * @deprecated use routerStore.redirect instead
+ * @param {string} path
+ * @param {boolean?} replace
  */
 export function redirect(path, replace) {
-    routerStore.redirect(path, replace);
+  routerStore.redirect(path, replace);
 }
 export { routerStore };
 export { default as Router } from "./Router.svelte";
